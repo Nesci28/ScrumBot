9a81d55caa6dfb3aff6953c6d6bdf4e0832d31c8
Marc G <marc@okidoo.ca>
2022-08-29 13:21:25 +0000

    feature: trolling

--- src/commands.ts
+++ src/commands.ts
@@ -2,7 +2,11 @@ import { TextChannel } from "discord.js";
 import { shuffle } from "lodash";
 
 export class Commands {
-  constructor(private readonly channel: TextChannel) {}
+  private isTroll = false;
+
+  constructor(private readonly channel: TextChannel) {
+    this.isTroll = process.env.TROLL_VINCENT === "true";
+  }
 
   public async createScrumOrder(usernames: string[]): Promise<void> {
     if (!this.channel) {
@@ -10,6 +14,16 @@ export class Commands {
     }
 
     const randomizedUsernames = shuffle(usernames);
-    this.channel.send(randomizedUsernames.join(" -> "));
+    const trolled = this.isTroll
+      ? this.trollVincent(randomizedUsernames)
+      : randomizedUsernames;
+    this.channel.send(trolled.join(" -> "));
+  }
+
+  private trollVincent(usernames: string[]): string[] {
+    const indexOfVincent = usernames.indexOf("Vincent");
+    usernames.splice(indexOfVincent, 1);
+    usernames.push("Vincent");
+    return usernames;
   }
 }
